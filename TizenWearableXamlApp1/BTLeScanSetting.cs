cffddf9e1eacbe990a593b410eeb1221d4ff76fe
vincent-kwon <hobum.kwon@gmail.com>
2020-04-02 07:17:58 +0900

    Add initial working sample of ImageButton.

---
+++ TizenWearableXamlApp1/BTLeScanSetting.cs
@@ -0,0 +1,48 @@
+﻿using System.Runtime.InteropServices;
+using Tizen;
+using Tizen.Network.Bluetooth;
+using System.Threading;
+
+namespace TizenWearableXamlApp1
+{
+    // NOTICE
+    // Use below BTConfigure.SetBLEScanning for scan mode of low latency.
+    // Must call BTConfigure.SetBLEScanning() just before scanning for safety because this method assumes BT initializtion.
+    public class BTConfigure
+    {
+        [DllImport("libcapi-network-bluetooth.so.0", EntryPoint = "bt_adapter_le_set_scan_mode")]
+        public static extern int SetLeScanMode(int mode);
+
+        public static void SetBLEScanning()
+        {
+            bool isEnabled = false;
+            isEnabled = BluetoothAdapter.IsBluetoothEnabled;
+            if (isEnabled)
+            {
+                Log.Info("VCApp", "Bluetooth is enabled");
+
+                // BT_ADAPTER_LE_SCAN_MODE_BALANCED 0 Balanced mode of power consumption and connection latency
+                // BT_ADAPTER_LE_SCAN_MODE_LOW_LATENCY 1 for low connection latency but high power consumption
+                // BT_ADAPTER_LE_SCAN_MODE_LOW_ENERGY 2 for low power consumption but high connection latency
+                SetLeScanMode(1);
+                Log.Info("VCApp", "Bluetooth SetLE Called. BT_ADAPTER_LE_SCAN_MODE_LOW_LATENCY");
+            }
+        }
+
+        public static void TestScanning()
+        {
+            /// For more information on scanning for BLE devices, see Managing Bluetooth LE Scans
+            BluetoothAdapter.ScanResultChanged += scanResultEventHandler;
+            BluetoothAdapter.StartLeScan();
+            Thread.Sleep(10000);
+            /// Wait while the system searches for the LE target you want to connect to
+            /// If you find the LE target you want, stop the scan
+            BluetoothAdapter.StopLeScan();
+        }
+
+        private static void scanResultEventHandler(object sender, AdapterLeScanResultChangedEventArgs e)
+        {
+            Log.Info("VCApp", "Bluetooth scanResultEventHandler Called.");
+        }
+    }
+}
---
+++ TizenWearableXamlApp1/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Tizen.Wearable.CircularUI.Forms;
+using System.Runtime.InteropServices;
+using Tizen;
+using Tizen.Network.Bluetooth;
+
+namespace TizenWearableXamlApp1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : CirclePage
+    {
+        [DllImport("libcapi-network-bluetooth.so.0", EntryPoint = "bt_adapter_le_set_scan_mode")]
+        public static extern int SetLeScanMode(int mode);
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BTConfigure.SetBLEScanning();
+            BTConfigure.TestScanning();
+        }
+
+        void OnImageButtonClicked(object sender, EventArgs e)
+        {
+            label.Text = "ImageButtonClicked.";
+        }
+    }
+}
